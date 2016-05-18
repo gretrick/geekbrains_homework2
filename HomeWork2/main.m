dfae060a5a685242f104b491e4462fb7c0300652
Sergejs Lastovskis <Sergejs.lastovskis@gmail.com>
2016-05-18 12:05:18 +0300

    Сделал дз

--- HomeWork2/main.m
+++ HomeWork2/main.m
@@ -8,10 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+void slFunction(){
+    NSString * fullName = @"Sergejs Lastovskis";
+    NSString * courseName;
+    courseName = @"Objective C. Уровень 1";
+    NSLog(@"Я студент %@.\nПрохожу бучение по курсу \"%@\"", fullName, courseName);
+
+    typedef enum {
+        PHP,
+        Swift,
+        ObjectiveC, // Не могу тут вписать "Objective C" с пробелом
+        CPlusPlus, // C++ Не могу вписать
+        
+    } programmingLanguages;
+    
+    programmingLanguages weLearn = 2;
+    NSLog(@"Количество возможных языков для изучения - %d", CPlusPlus+1);
+    
+    BOOL isLanguage = YES;
+    BOOL isProgram = YES;
+    BOOL isProgramLanguage = (isLanguage && isProgram) || (isLanguage || isProgram);
+    
+    BOOL isCircle = YES;
+    BOOL isRectangle = YES;
+    BOOL isCircleRectangle = (!isCircle && !isRectangle) || (!isCircle || !isRectangle);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        slFunction();
     }
     return 0;
 }
